f07a8f22036e53839b43ccbb78ce2991bea4adfc
TheMostOP <lcs3087@rit.edu>
2024-03-03 20:42:18 -0500

    updated title

--- src/htmlResponses.js
+++ src/htmlResponses.js
@@ -14,6 +14,6 @@ const getStyle = (request, response) => {
   response.writeHead(200, { 'Content-Type': 'text/css' });
   response.write(style);
   response.end();
-}; 
+};
 
 module.exports = { getIndex, getStyle };
